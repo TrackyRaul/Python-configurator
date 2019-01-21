0543d556caf7257805b933c4e640a7ab84b0421d
Raul <raul.farkas19@gmail.com>
2019-01-21 10:14:55 +0100

    Added example in tester script

--- tester.py
+++ tester.py
@@ -2,6 +2,7 @@ import configurator, json
 
 
 if __name__ == "__main__":
-    configurator.generate_config_structure("config.json")
-    configurator.configure("config.json")
+    ''' Example usage of the configurator '''
+    conf = configurator.configure("config.json")
+    print("User: %s\nPassword: %s\nHost: %s\nDatabase: %s"%(conf.db.user,conf.db.password,conf.db.host,conf.db.database))
     
